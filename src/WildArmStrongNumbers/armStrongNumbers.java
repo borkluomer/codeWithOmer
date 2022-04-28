5323f8b8d097333100cea991edd9f4616690ab7f
borkluomer <102393235+borkluomer@users.noreply.github.com>
2022-04-28 15:22:30 -0500

    wildproject

---
+++ src/WildArmStrongNumbers/armStrongNumbers.java
@@ -0,0 +1,29 @@
+package WildArmStrongNumbers;
+
+public class armStrongNumbers {
+
+    public static void main(String[] args) {
+
+        int number = 1634;
+        int len=0;
+        int myNumber=number;
+        int myNumber1=number;
+        int result=0;
+
+        while(number!=0){  //153
+            len++;
+            number= number/10;}
+        System.out.println(len);
+
+        while(myNumber!=0) {
+            int temp=1;
+            int lastdigit=myNumber%10;
+            for (int i = 1; i <=len ; i++) { temp *= lastdigit;}
+
+            result += temp;
+            myNumber=myNumber/10;}
+
+        if ( result == myNumber1 )  {System.out.println("arms");}
+        else  {System.out.println("not");}
+    }
+}
